51303f18371dcf14db82e427d8dfcbb67cf2999b
lokendra-singh-dhirawat <lokendra20048@gmail.com>
2025-08-21 17:50:05 +0000

    Initialize Express server with health check,morgan,dotenv and JSON parser

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import systemHealthCheckRouter from "./src/routes/systemHealthCheckRtr";
+dotenv.config();
+const app = express();
+
+app.use(morgan("dev"));
+
+app.set("json spaces", 5);
+app.use(express.json());
+
+app.use("/", systemHealthCheckRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
