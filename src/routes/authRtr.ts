9f5f280788352f40c2ab29fe0d3da6406d1e11bc
lokendra-singh-dhirawat <lokendra20048@gmail.com>
2025-08-22 05:10:53 +0000

    Add: authentication Router

---
+++ src/routes/authRtr.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { authCntrl } from "../controllers/authCntrl";
+import { validationMiddleware } from "../middleware/validateSchema";
+import { authenticated } from "../config/passport";
+
+import {
+  registerSchema,
+  loginSchema,
+  refreshTokenSchema,
+  changePasswordSchema,
+} from "../schema/authSchema";
+
+const router = Router();
+
+router.post(
+  "/register",
+  validationMiddleware.validateBody(registerSchema),
+  authCntrl.register
+);
+
+router.post(
+  "/login",
+  validationMiddleware.validateBody(loginSchema),
+  authCntrl.login
+);
+
+router.post(
+  "/refresh-token",
+  validationMiddleware.validateBody(refreshTokenSchema),
+  authCntrl.refreshToken
+);
+
+router.post("/logout", authenticated, authCntrl.logout);
+
+router.post(
+  "/change-password",
+  authenticated,
+  validationMiddleware.validateBody(changePasswordSchema),
+  authCntrl.changePassword
+);
+
+export default router;
