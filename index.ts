e00d02fdac77185abae73630b70fd981a7587a81
lokendra-singh-dhirawat <lokendra20048@gmail.com>
2025-08-25 04:56:23 +0000

    Add:socket.io in index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
+import { Server as Engine } from "@socket.io/bun-engine";
+import { Server } from "socket.io";
+import { ServerToClientEvents, ClientToServerEvents } from "./src/types/socket";
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import systemHealthCheckRouter from "./src/routes/systemHealthCheckRtr";
+import { loggers } from "winston";
+import logger from "./src/config/logger";
+
 dotenv.config();
 const app = express();
 
@@ -12,9 +18,28 @@ app.use(express.json());
 
 app.use("/", systemHealthCheckRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const io = new Server<ServerToClientEvents, ClientToServerEvents>();
+const engine = new Engine({
+  path: "/socket.io",
+});
+
+io.bind(engine);
+
+Bun.serve({
+  ...engine.handler(),
+  port: Number(process.env.PORT) || 3000,
+});
+
+io.on("connection", (socket) => {
+  logger.info("a user connected");
+  socket.on("message", (msg) => {
+    logger.info("message: " + msg);
+    io.emit("message", msg);
+  });
+});
+
+io.on("disconnect", (socket) => {
+  logger.info("a user disconnected");
 });
 
 export default app;
