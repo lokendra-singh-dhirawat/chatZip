75a2902d15e637fd49997e0e24392c9f85a31182
lokendra-singh-dhirawat <lokendra20048@gmail.com>
2025-09-01 04:27:24 +0000

    remove soket integration in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-import { Server as Engine } from "@socket.io/bun-engine";
-import { Server } from "socket.io";
-import { ServerToClientEvents, ClientToServerEvents } from "./src/types/socket";
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
@@ -8,6 +5,8 @@ import passport from "passport";
 import systemHealthCheckRouter from "./src/routes/systemHealthCheckRtr";
 import logger from "./src/config/logger";
 import { configurePassport } from "./src/config/configurePassport";
+import authRtr from "./src/routes/authRtr";
+import corsMiddleware from "./src/middleware/cors";
 
 dotenv.config();
 const app = express();
@@ -17,32 +16,17 @@ app.use(morgan("dev"));
 app.set("json spaces", 5);
 app.use(passport.initialize());
 configurePassport();
+
+app.set("trust proxy", 1);
+app.use(corsMiddleware);
+
 app.use(express.json());
 
 app.use("/", systemHealthCheckRouter);
+app.use("/auth", authRtr);
 
-const io = new Server<ServerToClientEvents, ClientToServerEvents>();
-const engine = new Engine({
-  path: "/socket.io",
-});
-
-io.bind(engine);
-
-Bun.serve({
-  ...engine.handler(),
-  port: Number(process.env.PORT) || 3000,
-});
-
-io.on("connection", (socket) => {
-  logger.info("a user connected");
-  socket.on("message", (msg) => {
-    logger.info("message: " + msg);
-    io.emit("message", msg);
-  });
-});
-
-io.on("disconnect", (socket) => {
-  logger.info("a user disconnected");
+app.listen(process.env.PORT || 3000, () => {
+  logger.info(`Server is running on port ${process.env.PORT || 3000}`);
 });
 
 export default app;
